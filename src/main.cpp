9aacb8be7daae24153013840f06b7f71c80aeaf2
Akihiro Nomura <nomu_san_2112_0903@outlook.com>
2020-11-29 13:46:54 +0900

    Save score into history.txt at the end of the game [#5]

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
@@ -6,6 +9,40 @@
 #include "messagebox.h"
 
 #define BGM_PATH "../music/bgm_maoudamashii_8bit07.wav"
+#define HISTORY_FILE_PATH "../history.txt"
+
+std::string GetCurrentTime()
+{
+  time_t t = time(nullptr);
+  const tm* lt = localtime(&t);
+
+  std::stringstream ss;
+  ss << "20" << lt->tm_year-100;
+  ss << "/";
+  ss << lt->tm_mon+1;
+  ss << "/";
+  ss << lt->tm_mday;
+  ss << " ";
+  ss << lt->tm_hour;
+  ss << ":";
+  ss << lt->tm_min;
+  ss << ":";
+  ss << lt->tm_sec;
+
+  return ss.str();
+}
+
+void WriteHistory(int score, int size, const char *file_path)
+{
+  std::ofstream writing_file(file_path);
+  if (!writing_file.is_open()) {
+    std::cout << "Cloud not open file..." << "\n";
+  }
+  writing_file << "Time: " << GetCurrentTime() << "\n";
+  writing_file << "Score: " << score << "\n";
+  writing_file << "Size: " << size << "\n";
+  writing_file.close();
+}
 
 int main() {
   constexpr std::size_t kFramesPerSecond{60};
@@ -21,6 +58,9 @@ int main() {
   MessageBox messagebox;
   Game game(kGridWidth, kGridHeight);
   game.Run(controller, renderer, bgm, messagebox, kMsPerFrame);
+
+  WriteHistory(game.GetScore(), game.GetSize(), HISTORY_FILE_PATH);
+
   std::cout << "Game has terminated successfully!\n";
   std::cout << "Score: " << game.GetScore() << "\n";
   std::cout << "Size: " << game.GetSize() << "\n";
